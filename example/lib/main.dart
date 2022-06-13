7f4904bc6e23396700328e0ec7f3a56d4e3dd139
Herbert Amukhuma <herbertamukhuma6@gmail.com>
2022-06-13 05:45:05 +0300

    Updated example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:example/model.dart';
 import 'package:flutter/material.dart';
+import 'package:scrollable_table_view/scrollable_table_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,11 +34,28 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    var columns = products.first.keys.toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(),
+      body: ScrollableTableView(
+        columns: columns.map((column) {
+          return TableViewColumn(
+            label: column,
+          );
+        }).toList(),
+        rows: products.map((product) {
+          return TableViewRow(
+            height: 60,
+            cells: columns.map((column) {
+              return TableViewCell(
+                child: Text(product[column] ?? ""),
+              );
+            }).toList(),
+          );
+        }).toList(),
+      ),
     );
   }
 }
