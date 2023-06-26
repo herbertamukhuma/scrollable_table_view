fdaa64cbcdeb0bc045eecfab8a49cc34d6cc6036
Herbert Amukhuma <herbertamukhuma6@gmail.com>
2023-06-26 14:57:41 +0300

    optimizing scrolling on mobile

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -48,113 +48,112 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     var columns = products.first.keys.toList();
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          bottom: const TabBar(
-            tabs: [
-              Tab(text: "Simple"),
-              Tab(text: "Paginated"),
-            ],
-          ),
+      appBar: AppBar(
+        title: Text(widget.title),
+        bottom: const TabBar(
+          tabs: [
+            Tab(text: "Simple"),
+            Tab(text: "Paginated"),
+          ],
         ),
-        body: TabBarView(
-          children: [
-            // simple
-            ScrollableTableView(
-              columns: columns.map((column) {
-                return TableViewColumn(
-                  label: column,
-                );
-              }).toList(),
-              rows: products.map((product) {
-                return TableViewRow(
-                  height: 60,
-                  cells: columns.map((column) {
-                    return TableViewCell(
-                      child: Text(product[column] ?? ""),
+      ),
+      body: TabBarView(
+        children: [
+          // simple
+          ScrollableTableView(
+            headers: columns.map((column) {
+              return TableViewHeader(
+                label: column,
+              );
+            }).toList(),
+            rows: products.map((product) {
+              return TableViewRow(
+                height: 60,
+                cells: columns.map((column) {
+                  return TableViewCell(
+                    child: Text(product[column] ?? ""),
+                  );
+                }).toList(),
+              );
+            }).toList(),
+          ),
+          // paginated
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: ValueListenableBuilder(
+                    valueListenable: _paginationController,
+                    builder: (context, value, child) {
+                      return Row(
+                        children: [
+                          Text(
+                              "${_paginationController.currentPage}  of ${_paginationController.pageCount}"),
+                          Row(
+                            children: [
+                              IconButton(
+                                onPressed:
+                                    _paginationController.currentPage <= 1
+                                        ? null
+                                        : () {
+                                            _paginationController.previous();
+                                          },
+                                iconSize: 20,
+                                splashRadius: 20,
+                                icon: Icon(
+                                  Icons.arrow_back_ios_new_rounded,
+                                  color: _paginationController.currentPage <= 1
+                                      ? Colors.black26
+                                      : Theme.of(context).primaryColor,
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: _paginationController.currentPage >=
+                                        _paginationController.pageCount
+                                    ? null
+                                    : () {
+                                        _paginationController.next();
+                                      },
+                                iconSize: 20,
+                                splashRadius: 20,
+                                icon: Icon(
+                                  Icons.arrow_forward_ios_rounded,
+                                  color: _paginationController.currentPage >=
+                                          _paginationController.pageCount
+                                      ? Colors.black26
+                                      : Theme.of(context).primaryColor,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      );
+                    }),
+              ),
+              Expanded(
+                child: ScrollableTableView(
+                  paginationController: _paginationController,
+                  headers: columns.map((column) {
+                    return TableViewHeader(
+                      label: column,
                     );
                   }).toList(),
-                );
-              }).toList(),
-            ),
-            // paginated
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25),
-                  child: ValueListenableBuilder(
-                      valueListenable: _paginationController,
-                      builder: (context, value, child) {
-                        return Row(
-                          children: [
-                            Text(
-                                "${_paginationController.currentPage}  of ${_paginationController.pageCount}"),
-                            Row(
-                              children: [
-                                IconButton(
-                                  onPressed:
-                                      _paginationController.currentPage <= 1
-                                          ? null
-                                          : () {
-                                              _paginationController.previous();
-                                            },
-                                  iconSize: 20,
-                                  splashRadius: 20,
-                                  icon: Icon(
-                                    Icons.arrow_back_ios_new_rounded,
-                                    color:
-                                        _paginationController.currentPage <= 1
-                                            ? Colors.black26
-                                            : Theme.of(context).primaryColor,
-                                  ),
-                                ),
-                                IconButton(
-                                  onPressed:
-                                      _paginationController.currentPage >=
-                                              _paginationController.pageCount
-                                          ? null
-                                          : () {
-                                              _paginationController.next();
-                                            },
-                                  iconSize: 20,
-                                  splashRadius: 20,
-                                  icon: Icon(
-                                    Icons.arrow_forward_ios_rounded,
-                                    color: _paginationController.currentPage >=
-                                            _paginationController.pageCount
-                                        ? Colors.black26
-                                        : Theme.of(context).primaryColor,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
+                  rows: many_products.map((product) {
+                    return TableViewRow(
+                      height: 60,
+                      cells: columns.map((column) {
+                        return TableViewCell(
+                          child: Text(product[column] ?? ""),
                         );
-                      }),
+                      }).toList(),
+                    );
+                  }).toList(),
                 ),
-                Expanded(
-                  child: ScrollableTableView(
-                    paginationController: _paginationController,
-                    columns: columns.map((column) {
-                      return TableViewColumn(
-                        label: column,
-                      );
-                    }).toList(),
-                    rows: many_products.map((product) {
-                      return TableViewRow(
-                        height: 60,
-                        cells: columns.map((column) {
-                          return TableViewCell(
-                            child: Text(product[column] ?? ""),
-                          );
-                        }).toList(),
-                      );
-                    }).toList(),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ));
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
